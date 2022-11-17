da2d150e10cd0476adfd69e11dd3ade06f7e2c8e
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-17 15:17:40 -0300

    feat: add intial config

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
+
+import { SignIn } from '@screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if (!fontsLoaded) {
+    <ActivityIndicator />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
+}
