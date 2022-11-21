a5a0b1fe7bdd9fd9ce3b00aed3412c7e91a8c110
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-21 17:04:31 -0300

    feat: finisg style screen Products

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import { Platform, TouchableOpacity, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
+import { InputPrice } from '@components/InputPrice';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 import {
   Container,
@@ -12,10 +15,21 @@ import {
   DeleteLabel,
   Upload,
   PickImageButton,
+  Form,
+  InputGroup,
+  Label,
+  InputGroupHeader,
+  MaxCharacters,
 } from './styles';
 
 export function Product() {
   const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [priceSizeP, setPriceSizeP] = useState('');
+  const [priceSizeM, setPriceSizeM] = useState('');
+  const [priceSizeG, setPriceSizeG] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handlePickerImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -42,14 +56,66 @@ export function Product() {
         </TouchableOpacity>
       </Header>
 
-      <Upload>
-        <Photo uri={image} />
-        <PickImageButton
-          title='Carregar'
-          type='secondary'
-          onPress={handlePickerImage}
-        />
-      </Upload>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Upload>
+          <Photo uri={image} />
+          <PickImageButton
+            title='Carregar'
+            type='secondary'
+            onPress={handlePickerImage}
+          />
+        </Upload>
+
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input
+              value={name}
+              onChangeText={setName}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>o de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+            <Input
+              multiline
+              maxLength={60}
+              style={{ height: 80 }}
+              value={description}
+              onChangeText={setDescription}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Tamanhos e preços</Label>
+
+            <InputPrice
+              size='P'
+              value={priceSizeP}
+              onChangeText={setPriceSizeP}
+            />
+            <InputPrice
+              size='M'
+              value={priceSizeM}
+              onChangeText={setPriceSizeM}
+            />
+            <InputPrice
+              size='G'
+              value={priceSizeG}
+              onChangeText={setPriceSizeG}
+            />
+          </InputGroup>
+
+          <Button
+            title='Cadastrar pizza'
+            isLoading={isLoading}
+          />
+
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
